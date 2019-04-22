317cd6bcb336b068b4a9dd8f94935b0f80a0a545
15029274098 <15029274098@163.com>
2019-04-22 22:43:40 +0800

    第一章微服务的注册与发现详解

---
+++ 第一章服务的注册与发现/spring-cloud-config-client/src/main/java/io/ymq/example/config/client/ConfigClientApplication.java
@@ -0,0 +1,32 @@
+package io.ymq.example.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableDiscoveryClient
+/**
+* @EnableEurekaClient
+ * @EnableDiscoveryClient
+ * 两个注解任选其一都是可以的
+*/
+public class ConfigClientApplication {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/")
+    public String home() {
+        return "port:" + port;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
